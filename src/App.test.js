4ee106b9f2646a069d43a3954821737e84d9e567
Rafael Fernandes <rafatexfr@gmail.com>
2022-03-02 17:38:24 +0100

    ejected the app + small extras

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is my litle Trello clone made only with vanilla react!/i);
   expect(linkElement).toBeInTheDocument();
 });
